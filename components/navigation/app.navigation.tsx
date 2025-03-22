eaa14ba276814c04bb09a9501c250c86d44d4e3e
Ka Ka Lưu <kacode357@gmail.com>
2025-03-22 22:59:05 +0700

    update

--- components/navigation/app.navigation.tsx
+++ components/navigation/app.navigation.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../Screen/Home';
+import HomeScreen from '../Screen/home';
 
 const AppNavigation = () => {
   const Stack = createNativeStackNavigator();
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="home" component={HomeScreen} />
     </Stack.Navigator>
   );
 };
